b5f3e806c9cca50a9a5a5ea089436dfca0adbd86
NannaMarcher <nanna.marcher@gmail.com>
2021-02-11 12:22:21 +0100

    transferred connect_db, init_db, query_db, format_datetime and gravatar_url from py to go

--- src/minitwit.go
+++ src/minitwit.go
@@ -1,18 +1,56 @@
 package main
 
 import (
+	"crypto/sha1"
 	"database/sql"
+	"encoding/hex"
 	"fmt"
 	"net/http"
+	"strings"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var DB *sql.DB
-/*func connect_db() {
-	db, err := sql.Open("sqlite3", "../minitwit.db")
+const database string = "../minitwit.db"
+const per_page int = 30
+const debug bool = true
+const secret_key string = "development key"
+
+var DB *sql.DB = connect_db()
+
+func connect_db() (DB *sql.DB) {
+	db, err := sql.Open("sqlite3", database)
 	checkErr(err)
-    }*/
+	return db
+}
+
+func init_db() {
+	//unsure if we are already doing this in connect_db()
+}
+
+func query_db(query string, args string, one bool) (*sql.Rows, error) {
+	stmt, err := DB.Prepare(query)
+	checkErr(err)
+	defer stmt.Close()
+	rows, err := stmt.Query(args)
+	return rows, err
+}
+
+func format_datetime(timestamp string) string {
+	const layout = "2016-03-28 @ 08:30"
+	t, err := time.Parse(layout, timestamp)
+	checkErr(err)
+	return t.String()
+}
+
+func gravatar_url(email string) string {
+	size := 80
+	h := sha1.New()
+	h.Write(strings.ToLower(strings.TrimSpace(email)))
+	sha1_hash := hex.EncodeToString(h.Sum(nil))
+	return fmt.Sprintf("http://www.gravatar.com/avatar/%s?d=identicon&s=%d", sha1_hash, size)
+}
 
 func checkErr(err error) {
 	if err != nil {
@@ -20,27 +58,20 @@ func checkErr(err error) {
 	}
 }
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
-}
-
 func booksIndex(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open("sqlite3", "../minitwit.db")
+	rows, err := query_db("Select * from user where username = ?", "a", false)
 	checkErr(err)
-    rows, err := db.Query("Select * from user")
-    checkErr(err)
-
-    for rows.Next() {
+	defer rows.Close()
+	for rows.Next() {
 		var username string
 		var user_id string
 		var email string
 		var pw_hash string
-        err := rows.Scan(&user_id,&username,&email,&pw_hash)
+		err := rows.Scan(&user_id, &username, &email, &pw_hash)
 		checkErr(err)
 		fmt.Fprintln(w, username)
 	}
-}     
-
+}
 
 func main() {
 	http.HandleFunc("/", booksIndex)
