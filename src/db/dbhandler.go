623f757f38b43a84e598456035a64236fe82f7a1
parallels <laura@lunddahl.com>
2021-02-17 10:39:42 +0100

    dbhandler can now get data from mysql db

--- src/db/dbhandler.go
+++ src/db/dbhandler.go
@@ -7,6 +7,23 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+type User struct {
+	//gorm.Model
+	UserId   int
+	Username string
+	Email    string
+	PwHash   string
+}
+
+type Message struct {
+	//gorm.Model
+	MessageId int
+	AuthorId  int
+	Text      string
+	PubDate   int
+	Flagged   int
+}
+
 func main() {
 	db, err := gorm.Open("mysql", "groupf:f0psD3v1123@(localhost)/minitwit?charset=utf8&parseTime=True&loc=Local")
 	defer db.Close()
@@ -14,4 +31,23 @@ func main() {
 		fmt.Println(err)
 		panic("Failed to connect to the database!")
 	}
+
+	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Message{})
+
+	var user []User
+	db.Find(&user)
+	fmt.Println("{}", user)
+
+	var message []Message
+	db.Find(&message)
+	fmt.Println("{}", message)
+}
+
+func (User) TableName() string {
+	return "user"
+}
+
+func (Message) TableName() string {
+	return "message"
 }
