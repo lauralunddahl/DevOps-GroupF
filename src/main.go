fda66379186f909eb1be36b43092eb71df7a7315
NannaMarcher <nanna.marcher@gmail.com>
2021-02-17 16:13:04 +0100

    added query functions to message and user

--- src/main.go
+++ src/main.go
@@ -1,10 +1,11 @@
 package main
 
-import "dto"
-
 func main() {
-	//user := dto.GetUserID("laulu")
-	dto.GetUsers()
-	timeline := dto.GetPrivateTimeline(1)
-	println(timeline)
+	//dto.AddMessage("2", "hej med dig", 1, 1)
+	//dto.RegisterUser("user2", "user2@itu.dk", "39dfa55283318d31afe5a3ff4a0e3253e2045e43")
+	//laulu := dto.GetUserID("laulu")
+	//println(laulu)
+	//user := dto.GetUser("user1")
+	//timeline := dto.GetPrivateTimeline(2)
+	//println(timeline)
 }
