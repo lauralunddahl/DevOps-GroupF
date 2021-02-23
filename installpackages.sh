9ee78877b0ee9863e223f52f3dea57288e481a8b
NannaMarcher <47529979+NannaMarcher@users.noreply.github.com>
2021-02-23 15:35:57 +0100

    Create installpackages.sh

---
+++ installpackages.sh
@@ -0,0 +1,8 @@
+sudo apt-get install golang
+
+go get "github.com/gorilla/mux"
+go get "github.com/gorilla/sessions"
+go get "golang.org/x/crypto/bcrypt"
+go get "tawesoft.co.uk/go/dialog"
+go get "github.com/go-sql-driver/mysql"
+go get "github.com/jinzhu/gorm"
